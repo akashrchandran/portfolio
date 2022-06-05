4c38f7f1b99de2799d259fb83c2b2cb6ba6b0f4f
akashrchandran <chandranrakash@gmail.com>
2022-06-05 12:35:27 +0530

    added scroll to top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,3 +79,26 @@ function sendEmail() {
         });
     }
 }
+
+// Scroll to top
+let mybutton = document.getElementById("btn-back-to-top");
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 30 ||
+    document.documentElement.scrollTop > 30
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}

eb13c2f0de6bf181f3bc7d0a3a8d31320925bb49
Akshit Sangwan <akshitsangwan10@gmail.com>
2022-09-29 05:31:38 +0530

    Issue solved!
    Added functionality for smartphones to enable touch response(with no arrow navigators).
    For this:
    1. I added some JQuery for enabling the touch response and senstivity.
    2. I have also added some bootstrap classes for arrow buttons
    
    I have also fixed some typos and added some comments related to the functionalities that i added in your code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,6 +35,24 @@ var options = {
 
 var typed = new Typed('.typerjs', options);
 
+// JQuery for Certificates' Section
+$('.carousel').on('touchstart', function(event){
+    const xClick = event.originalEvent.touches[0].pageX;
+    $(this).one('touchmove', function(event){
+        const xMove = event.originalEvent.touches[0].pageX;
+        const sensitivityInPx = 5;
+
+        if( Math.floor(xClick - xMove) > sensitivityInPx ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < -sensitivityInPx ){
+            $(this).carousel('prev');
+        }
+    });
+    $(this).on('touchend', function(){
+        $(this).off('touchmove');
+    });
+});
 
 // Send Email func, should have used fetch but jquery seems to be nice
 emailbutton = document.getElementById('sendmail');

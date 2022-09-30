6b164ddfee50fca43ae888bc7f060f5866adb639
RahulSankhla312 <rahulsankhla312@gmail.com>
2022-09-30 16:01:09 +0530

    portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -104,116 +104,160 @@ function backToTop() {
   document.documentElement.scrollTop = 0;
 }
 
-{
-    var i = 0;
-      	var bar = document.querySelector(".progress-bar1");
-        function makeProgress(){
-            if(i < 90){
-                i = i + 0.25;
-              	bar.style.width = i + "%";
-              	//bar.innerText = i + "%";
-            }
+// // {
+// //     var i = 0;
+// //     var bar = document.querySelector(".progress-bar1");
+// //         function makeProgress(){
+// //             if(i < 90){
+// //                 i = i + 0.25;
+// //               	bar.style.width = i + "%";
+// //               	//bar.innerText = i + "%";
+// //             }
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress()", 10);
-        }
-        makeProgress();
-    }   
-{var j = 0;
-      	var bar1 = document.querySelector(".progress-bar2")
-        function makeProgress1(){
-            if(j < 75){
-                j = j + 0.25;
-              	bar1.style.width = j + "%";
-              	//bar.innerText = i + "%";
-            }
+// //             // Wait for sometime before running this script again
+// //             setTimeout("makeProgress()", 10);
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress1()", 10);
-        }
-        makeProgress1();
-    }
-{var a = 0;
-      	var bar2 = document.querySelector(".progress-bar3")
-        function makeProgress2(){
-            if(a < 60){
-                a = a + 0.25;
-              	bar2.style.width = a + "%";
-              	//bar.innerText = i + "%";
-            }
+// //         }
+// //         makeProgress();
+// //     }   
+// {var j = 0;
+//       	var bar1 = document.querySelector(".progress-bar2")
+//         function makeProgress1(){
+//             if(j < 75){
+//                 j = j + 0.25;
+//               	bar1.style.width = j + "%";
+//               	//bar.innerText = i + "%";
+//             }
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress2()", 10);
-        }
-        makeProgress2();
-    }
-{var b = 0;
-      	var bar3 = document.querySelector(".progress-bar4")
-        function makeProgress3(){
-            if(b < 60){
-                b = b + 0.25;
-              	bar3.style.width = b + "%";
-              	//bar.innerText = i + "%";
-            }
+//             // Wait for sometime before running this script again
+//             setTimeout("makeProgress1()", 10);
+//         }
+//         makeProgress1();
+//     }
+// {var a = 0;
+//       	var bar2 = document.querySelector(".progress-bar3")
+//         function makeProgress2(){
+//             if(a < 60){
+//                 a = a + 0.25;
+//               	bar2.style.width = a + "%";
+//               	//bar.innerText = i + "%";
+//             }
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress3()", 10);
-        }
-        makeProgress3();
-    }
-{var c = 0;
-      	var bar4 = document.querySelector(".progress-bar5")
-        function makeProgress4(){
-            if(c < 25){
-                c = c + 0.25;
-              	bar4.style.width = c + "%";
-              	//bar.innerText = i + "%";
-            }
+//             // Wait for sometime before running this script again
+//             setTimeout("makeProgress2()", 10);
+//         }
+//         makeProgress2();
+//     }
+// {var b = 0;
+//       	var bar3 = document.querySelector(".progress-bar4")
+//         function makeProgress3(){
+//             if(b < 60){
+//                 b = b + 0.25;
+//               	bar3.style.width = b + "%";
+//               	//bar.innerText = i + "%";
+//             }
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress4()", 10);
-        }
-        makeProgress4();
-    }
-{var d = 0;
-      	var bar5 = document.querySelector(".progress-bar6")
-        function makeProgress5(){
-            if(d < 90){
-                d = d + 0.25;
-              	bar5.style.width = d + "%";
-              	//bar.innerText = i + "%";
-            }
+//             // Wait for sometime before running this script again
+//             setTimeout("makeProgress3()", 10);
+//         }
+//         makeProgress3();
+//     }
+// {var c = 0;
+//       	var bar4 = document.querySelector(".progress-bar5")
+//         function makeProgress4(){
+//             if(c < 25){
+//                 c = c + 0.25;
+//               	bar4.style.width = c + "%";
+//               	//bar.innerText = i + "%";
+//             }
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress5()", 10);
-        }
-        makeProgress5();
-    }
-{var e = 0;
-      	var bar6 = document.querySelector(".progress-bar7")
-        function makeProgress6(){
-            if(e < 75){
-                e = e + 0.25;
-              	bar6.style.width = e + "%";
-              	//bar.innerText = i + "%";
-            }
+//             // Wait for sometime before running this script again
+//             setTimeout("makeProgress4()", 10);
+//         }
+//         makeProgress4();
+//     }
+// {var d = 0;
+//       	var bar5 = document.querySelector(".progress-bar6")
+//         function makeProgress5(){
+//             if(d < 90){
+//                 d = d + 0.25;
+//               	bar5.style.width = d + "%";
+//               	//bar.innerText = i + "%";
+//             }
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress6()", 10);
-        }
-        makeProgress6();
-    }
-{var f = 0;
-      	var bar7 = document.querySelector(".progress-bar8")
-        function makeProgress7(){
-            if(f < 40){
-                f = f + 0.25;
-              	bar7.style.width = f + "%";
-              	//bar.innerText = i + "%";
-            }
+//             // Wait for sometime before running this script again
+//             setTimeout("makeProgress5()", 10);
+//         }
+//         makeProgress5();
+//     }
+// {var e = 0;
+//       	var bar6 = document.querySelector(".progress-bar7")
+//         function makeProgress6(){
+//             if(e < 75){
+//                 e = e + 0.25;
+//               	bar6.style.width = e + "%";
+//               	//bar.innerText = i + "%";
+//             }
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress7()", 10);
+//             // Wait for sometime before running this script again
+//             setTimeout("makeProgress6()", 10);
+//         }
+//         makeProgress6();
+//     }
+// {var f = 0;
+//       	var bar7 = document.querySelector(".progress-bar8")
+//         function makeProgress7(){
+//             if(f < 40){
+//                 f = f + 0.25;
+//               	bar7.style.width = f + "%";
+//               	//bar.innerText = i + "%";
+//             }
+
+//             // Wait for sometime before running this script again
+//             setTimeout("makeProgress7()", 10);
+//         }
+//         makeProgress7();
+//     }
+
+//     let skils = document.getElementById(".nav-link");
+
+//         skils.addEventListener("click", RestartProgressBar);
+//         function RestartProgressBar(){
+//             if(skils.textContent === "Skills" ){
+//                 i = 0;
+//                 makeProgress();
+//             }
+//         }
+    
+        
+const skillSection = document.getElementById('pills-skill-tab');
+const progressBars = document.querySelectorAll('.progress-bar');
+
+function showProgress() {
+    progressBars.forEach(progressBar => {
+        const value = progressBar.dataset.progress;
+        progressBar.style.opacity = 1;
+        progressBar.style.width = `${value}%`;
+    });
+}
+
+function hideProgress(){
+    progressBars.forEach(p => {
+        p.style.opacity = 0;
+        p.style.width = 0;
+
+});
+
+}
+
+window.addEventListener('scroll', () => {
+    const sectionPos = skillSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight/2;
+
+
+    if(sectionPos < screenPos){
+        showProgress();}
+        else{
+            hideProgress();
         }
-        makeProgress7();
-    }
+});

87aa65a61201927d2eefb304ac1fcdd6b0b17ba0
Akash R Chandran <chandranrakash@gmail.com>
2023-01-14 17:38:47 +0530

    added pop message if captcha isn't verified

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,47 +60,48 @@ $('#contact-form').submit((e)=>{
 
 // Send Email func, should have used fetch but jquery seems to be nice
 emailbutton = document.getElementById('sendmail');
+emailinfo = document.getElementById('form-info');
 function sendEmail() {
+    const response = grecaptcha.getResponse();
+    if (!response) {
+        emailinfo.innerHTML = '<i class="fas fa-exclamation-circle"></i> Please verify the captcha';
+        return;
+    }
+    else {
+        emailinfo.innerHTML = '';
+    }
     emailbutton.innerHTML = '<i class="fa fa-spinner fa-spin"></i> Loading...';
     emailbutton.classList.add("disabled_def");
     const name = document.getElementById("form-name");
     const phone = document.getElementById("form-phone");
     const email = document.getElementById("form-email");
     const message = document.getElementById("form-message");
-    const response = grecaptcha.getResponse();
-    if (!name.value || !email.value || !message.value || !response) {
-        emailbutton.style = "background: red";
-        emailbutton.innerHTML = '<i class="fa fa-times"></i> Failed';
+    var data = {
+        service_id: 'service_9fnrphq',
+        template_id: 'template_ie65z47',
+        user_id: 'XCfDptyHXB-M91Vj3',
+        template_params: {
+            name: name.value,
+            email: email.value,
+            phone: phone.value,
+            message: message.value,
+            'g-recaptcha-response': response
+        }
+    };
+    emailbutton.style = "cursor: pointer; pointer-events: none;";
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
+        type: 'POST',
+        data: JSON.stringify(data),
+        contentType: 'application/json'
+    }).done(function () {
+        emailbutton.innerHTML = '<i class="fa fa-check"></i> Sent';
+        emailbutton.style = "background: green";
         setTimeout(location.reload.bind(location), 2000);
-    }
-    else {
-        var data = {
-            service_id: 'service_9fnrphq',
-            template_id: 'template_ie65z47',
-            user_id: 'XCfDptyHXB-M91Vj3',
-            template_params: {
-                name: name.value,
-                email: email.value,
-                phone: phone.value,
-                message: message.value,
-                'g-recaptcha-response': response
-            }
-        };
-        emailbutton.style = "cursor: pointer; pointer-events: none;";
-        $.ajax('https://api.emailjs.com/api/v1.0/email/send', {
-            type: 'POST',
-            data: JSON.stringify(data),
-            contentType: 'application/json'
-        }).done(function () {
-            emailbutton.innerHTML = '<i class="fa fa-check"></i> Sent';
-            emailbutton.style = "background: green";
-           setTimeout(location.reload.bind(location), 2000);
-        }).fail(function (error) {
-            emailbutton.innerHTML = '<i class="fa fa-times"></i> Failed';
-            emailbutton.style = "background: red";
-            setTimeout(location.reload.bind(location), 2000);
+    }).fail(function (error) {
+        emailbutton.innerHTML = '<i class="fa fa-times"></i> Failed';
+        emailbutton.style = "background: red";
+        setTimeout(location.reload.bind(location), 2000);
         });
-    }
 }
 
 // Scroll to top
@@ -161,3 +162,4 @@ window.addEventListener('scroll', () => {
             hideProgress();
     }
 });
+  
